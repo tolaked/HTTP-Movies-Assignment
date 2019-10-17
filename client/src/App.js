879c7f0038ffdc8b90bf303ae15c9dbe5855b644
Akere Adetola <adetolaakere1@gmail.com>
2019-10-17 14:30:33 +0100

    created updateForm component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
+import { UserProvider } from "./Movies/userContext";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
@@ -13,14 +14,22 @@ const App = () => {
 
   return (
     <>
-      <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
-      <Route
-        path="/movies/:id"
-        render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
-        }}
-      />
+      <UserProvider savedList={savedList}>
+        <SavedList list={savedList} />
+        <Route exact path="/" component={MovieList} />
+        <Route
+          path="/movies/:id"
+          render={props => {
+            return <Movie {...props} addToSavedList={addToSavedList} />;
+          }}
+        />
+        <Route
+          path={`/update-movie/:id`}
+          render={props => {
+            return <MovieList {...props} addToSavedList={addToSavedList} />;
+          }}
+        />
+      </UserProvider>
     </>
   );
 };
